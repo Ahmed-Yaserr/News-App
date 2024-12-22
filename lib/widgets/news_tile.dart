8690d34cbee91de00b3d801c322ed6e128b45ff8
Ahmedyas94 <mekko234@gmail.com>
2024-12-22 14:46:45 +0200

    final edit

--- lib/widgets/news_tile.dart
+++ lib/widgets/news_tile.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:news_app/models/article_model.dart';
 
 import '../views/webView.dart';
 
-// cached network image
 class NewsTile extends StatelessWidget {
   const NewsTile({super.key, required this.articleModel});
 
   final ArticleModel articleModel;
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -23,13 +24,32 @@ class NewsTile extends StatelessWidget {
         mainAxisSize: MainAxisSize.min,
         children: [
           ClipRRect(
-              borderRadius: BorderRadius.circular(6),
-              child: Image.network(
-                articleModel.image !,
+            borderRadius: BorderRadius.circular(6),
+            child: CachedNetworkImage(
+              imageUrl: articleModel.image ?? '',
+              height: 200,
+              width: double.infinity,
+              fit: BoxFit.cover,
+              placeholder: (context, url) => Container(
                 height: 200,
                 width: double.infinity,
-                fit: BoxFit.cover,
-              )),
+                color: Colors.grey[200],
+                child: const Center(
+                  child: CircularProgressIndicator(),
+                ),
+              ),
+              errorWidget: (context, url, error) => Container(
+                height: 200,
+                width: double.infinity,
+                color: Colors.grey[200],
+                child: const Icon(
+                  Icons.broken_image,
+                  color: Colors.grey,
+                  size: 50,
+                ),
+              ),
+            ),
+          ),
           const SizedBox(
             height: 12,
           ),
@@ -50,7 +70,7 @@ class NewsTile extends StatelessWidget {
             articleModel.subTitle ?? '',
             maxLines: 2,
             style: const TextStyle(color: Colors.grey, fontSize: 14),
-          )
+          ),
         ],
       ),
     );
